f5fc60acc50b7dcd45d0ddf79c3dd12153411413
chainerprince <mutegetsi2000@gmail.com>
2023-06-12 20:35:26 +0200

    feat: add signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import NotFound from './pages/NotFound';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="" element={<Login />} />
+        <Route exact path="/register" element={<SignUp />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route element={<NotFound />} />
       </Routes>
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,21 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-
-export const LoginForm = () => {
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+const schema = yup.object().shape({
+  firstName: yup.string().required("First name is required"),
+  lastName: yup.string().required("Last name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup.string().min(8).max(32).required("The password is required"),
+});
+export const SignUpForm = () => {
+ const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) =>{ 
+    console.log(data)
+    reset();
+}
   return (
     <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -11,9 +26,35 @@ export const LoginForm = () => {
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Sign in to your account
+              Create an account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900">Your Firstname</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="My name"
+                  required=""
+                  {...register("firstName")}
+                />
+                <p className="text-red-700">{errors.firstName?.message}</p>
+              </div>
+              <div>
+                <label htmlFor="lastName" className="block mb-2 text-sm font-medium text-gray-900">Your Lastname</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="My last name"
+                  required=""
+                  {...register("lastName")}
+                />
+                <p className="text-red-700">{errors.lastName?.message}</p>
+              </div>
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
                 <input
@@ -23,7 +64,9 @@ export const LoginForm = () => {
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="name@company.com"
                   required=""
+                  {...register("email")}
                 />
+                <p className="text-red-700">{errors.email?.message}</p>
               </div>
               <div>
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
@@ -34,7 +77,9 @@ export const LoginForm = () => {
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   required=""
+                  {...register("password")}
                 />
+                <p className="text-red-700">{errors.password?.message}</p>
               </div>
               <div className="flex items-center justify-between">
                 <div className="flex items-start">
@@ -57,10 +102,10 @@ export const LoginForm = () => {
                 type="submit"
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
-                Sign in
+                Register
               </button>
               <p className="text-sm font-light text-gray-500">
-                Don’t have an account yet? <Link to="/register" className="font-medium text-primary-600 hover:underline">Sign up</Link>
+                Already have an account? <Link to="/" className="font-medium text-primary-600 hover:underline">Sign in</Link>
               </p>
             </form>
           </div>
